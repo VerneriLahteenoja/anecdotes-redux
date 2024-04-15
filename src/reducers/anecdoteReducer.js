f3ba11caf51daf7c26c96a9b4263a03bb6f300e9
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-04-15 22:00:06 +0300

    rename anecdote reducer to anecdoteReducer

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -37,7 +37,7 @@ export const createAnecdote = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
+const anecdoteReducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
   switch (action.type) {
@@ -54,4 +54,4 @@ const reducer = (state = initialState, action) => {
 
 }
 
-export default reducer
+export default anecdoteReducer
