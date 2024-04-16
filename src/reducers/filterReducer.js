55c1b0f9762e48d0164c76d425883116199cdcd8
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-04-16 23:55:45 +0300

    refactor reducer to filterSlice

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -1,16 +1,18 @@
-export const filterBy = (filterBy) => {
-  return {
-    type: 'SET_FILTER',
-    payload: filterBy
-  }
-}
+import { createSlice } from "@reduxjs/toolkit"
 
-const filterReducer = (state = '', action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
+
+const initialState = ''
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    filterBy(state, action) {
       return [action.payload]
-    default: return state
+    }
   }
-}
+})
 
-export default filterReducer
+export const { filterBy } = filterSlice.actions
+
+export default filterSlice.reducer
